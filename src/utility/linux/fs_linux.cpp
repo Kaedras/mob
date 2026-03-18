2b90f2147d22924667ef4845c2cf1b1e74f3e709
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-03-18 23:07:21 +0100

    Improve `make_temp_file()` to not use a variable-length array

--- src/utility/linux/fs_linux.cpp
+++ src/utility/linux/fs_linux.cpp
@@ -2,19 +2,21 @@
 #include "../../core/context.h"
 #include "../fs.h"
 
+using namespace std;
+
 namespace mob {
 
     fs::path make_temp_file()
     {
         static fs::path dir = conf().path().temp_dir();
 
-        std::string templ = dir / "tmpXXXXXX";
+        const string templateString = dir / "tmpXXXXXX";
 
         // mkstemp requires a modifiable char array
-        char filename[templ.length() + 1];
-        strcpy(filename, templ.c_str());
+        const auto path = make_unique<char[]>(templateString.length() + 1);
+        strcpy(path.get(), templateString.c_str());
 
-        int fd = mkstemp(filename);
+        const int fd = mkstemp(path.get());
 
         if (fd == -1) {
             const auto e = errno;
@@ -26,7 +28,7 @@ namespace mob {
         // Close the file descriptor since we only need the name
         close(fd);
 
-        return std::filesystem::path(filename);
+        return std::filesystem::path(path.get());
     }
 
 }  // namespace mob
